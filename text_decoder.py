a7d3590ba5c81ccd39220c3bbdae3402923f1d6b
M Abdullah Khan <khanabd@umich.edu>
2025-05-08 02:40:34 -0400

    Testing and fixing output

--- text_decoder.py
+++ text_decoder.py
@@ -1,6 +1,5 @@
 import random
 import sys
-from text_encoder import pos_rotation
 
 # letter list
 alph_lst = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -18,9 +17,11 @@ shuf_lst_3 = ['V', 'D', 'S', 'K', 'R', 'E', 'H', 'C', 'W', 'P', 'J', 'Q', 'L', '
 dict_1 = {}
 for pos in range(len(alph_lst)):
     dict_1[pos] = shuf_lst_1[pos]
+
 dict_2 = {}
 for pos in range(len(alph_lst)):
     dict_2[pos] = shuf_lst_2[pos]
+
 dict_3 = {}
 for pos in range(len(alph_lst)):
     dict_3[pos] = shuf_lst_3[pos]
@@ -36,21 +37,38 @@ def reverse_pos_rotation(pos_dict):
             new_dict[num-1] = char
         elif num == 0:
             new_dict[25] = char
+
+    
     return new_dict
 
 # The reflector
 shuf_lst_4 = ['P', 'G', 'L', 'E', 'F', 'S', 'U', 'J', 'Z', 'A', 'H', 'V', 'M', 'R', 'W', 'D', 'B', 'X', 'I', 'C', 'O', 'T', 'Y', 'N', 'Q', 'K']
+
+
 pair_lst_1 = shuf_lst_4[:13]
 pair_lst_2 = shuf_lst_4[13:]
-reflector_pairs = list(zip(pair_lst_1, pair_lst_2)) # Like the rotors, could not find data on reflector pairs. Can modify if data available
+reflector_pairs = list(zip(pair_lst_1, pair_lst_2))
 
-def reflector_encryption(letter, pairs=reflector_pairs): # Returns the encoded letter given an input letter and reflector pairings
+def reflector_encryption(letter, pairs=reflector_pairs):
     for pair in pairs:
         if letter in pair:
             for i in pair:
                 if i!=letter:
                     return i
     return None
+
+def pos_rotation(pos_dict):
+    new_dict = {}
+    for i in pos_dict.items():
+        num = i[0]
+        char = i[1]
+
+        if num == 25:
+            new_dict[0] = char
+        else:
+            new_dict[num+1] = char
+    
+    return new_dict
 
 # Making a function that sets the rotor dictionary states based on the length of the text during decryption
 def decryption_rotor_stateset(text, first_dict, second_dict, third_dict):
@@ -72,6 +90,7 @@ def text_rotary_decryption(text, first_dict=dict_1, second_dict=dict_2, third_di
 
     # Setting the states
     first_dict, second_dict, third_dict = decryption_rotor_stateset(text, first_dict, second_dict, third_dict)
+
     alph_lst = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     chr_count = -1
@@ -100,13 +119,12 @@ def text_rotary_decryption(text, first_dict=dict_1, second_dict=dict_2, third_di
         second_chr_first = alph_lst[second_index]
         if chr_count==len(text)%26 or chr_count%26==len(text)%26:
             second_dict = reverse_pos_rotation(second_dict)
-
+        
         first_index = list(first_dict.keys())[list(first_dict.values()).index(second_chr_first)]
         origin_char = alph_lst[first_index]
         first_dict = reverse_pos_rotation(first_dict)
 
+
         decrypted_text+=origin_char
 
     return decrypted_text[::-1]
-
-text_rotary_decryption('VHEAVOYIDTOEYAMPPLFIZJTDEZRENAOFDCCOYTTYHZLNNKPQQKUBJAFPGULOKJNGOTTERGRTNVJZSPNSVFVMDXRKFAHXUTCYYQMYFDTPFNYIEHMUWWDCCMSABEJZVKDDSYEONJOINMSQFNBYOIJLWDDSJIKJFIDYYBZIJGQUZFDISISUJZALBBYXARODISSQWUQHJRMYLKWODFRGJQKWDISKKMLZBGYRYQNQKLXSKFOKGTUISHPAGQAEVIHRORPEQFZSRBJHZTWXKLGCDPRDPQUNWQNJIMOPPXIJPWWYFTSPOPSWJUBWGJAAJDQPBVJJHTISVJAAFXDDBIXZOJMLNLIFHZHRTMQDQPLFFUUAJVMAUBEPVNUGDXCEJGUJAAJKMNJUDTLIMBCCVSWQZXKROLBQBRZZIMTQMTWTXZSSLBSYWHYITAQTRJDUHOXYDJZFVCUHWWIVJGGFAFDNWJTIKVAUKCAXQJIJARZCQLVHN')
